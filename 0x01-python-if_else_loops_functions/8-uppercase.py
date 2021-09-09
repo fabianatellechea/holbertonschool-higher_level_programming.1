5835b38c3e0b0aaa852166695dba887927d1ef0f
fabianatellechea <fabianatellechea@github.com>
2021-09-09 16:24:39 +0000

    :

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    
+    d = ord("a") - ord("A")
+    for c in str:
+        if ord(c) > 96 and ord(c) < 123:
+            c = ord(c) - d
+            print("{}".format(chr(c)), end="")
+        else:
+            print("{}".format(c), end="")
+    print()

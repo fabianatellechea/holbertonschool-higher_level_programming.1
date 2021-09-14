73097c39393d3648d73aeb0cb89ac826fe6dc12a
fabianatellechea <fabianatellechea@github.com>
2021-09-14 23:52:15 +0000

    .

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+
+    i = len(my_list)
+    new_list = list(my_list)
+
+    if idx < 0 or idx > i:
+        return my_list
+    else:
+        new_list[idx] = element
+        return new_list

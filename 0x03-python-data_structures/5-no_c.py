484f8eb4ca5746e7b1696dd11c02e4ed59c8848b
fabianatellechea <fabianatellechea@github.com>
2021-09-20 16:05:34 +0000

    :

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    list = ['c', 'C']
-    str2 = "".join(i for i in my_string if i not in list)
-    print(str2)
+    return my_string.translate({ord('c'): None, ord('C'): None})

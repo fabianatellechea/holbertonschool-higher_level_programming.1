34674aa252eb412b83e1cf2beff22d5d04dd7755
fabianatellechea <fabianatellechea@github.com>
2021-09-21 14:43:49 +0000

    :

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     max = my_list[0]
-    if my_list:
-        for x in my_list:
-            if x > max:
-                max = x
-        return max
     if not my_list:
         return None
+    for x in my_list:
+        if x > max:
+            max = x
+    return max

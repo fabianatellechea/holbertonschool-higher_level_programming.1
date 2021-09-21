033f771cbb0c72969fec9905a877a33194151868
fabianatellechea <fabianatellechea@github.com>
2021-09-21 14:15:14 +0000

    :

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple = (len(sentence), sentence[0])
-    if sentence == '\0':
+    if sentence:
+        tuple = (len(sentence), sentence[0])
+    else:
         tuple = (len(sentence), None)
     return tuple

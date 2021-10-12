7677aca043272c3c488ec24551814ed235726cc2
fabianatellechea <fabianatellechea@github.com>
2021-10-12 21:51:16 +0000

    :

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+"""class MyList that inherits from list"""
+
+
 class MyList(list):
+    """prints the list, but sorted (ascending sort)"""
+
     def print_sorted(self):
         print(sorted(self))

93133ad0ffcd1ef13a906ac44c3e0cc41bdad2ba
fabianatellechea <fabianatellechea@github.com>
2021-10-13 22:15:00 +0000

    .

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-"""function that writes a string to a text file (UTF8) and returns the number of characters written"""
+"""function that writes a string to a text file
+(UTF8) and returns the number of characters written"""
 
 
 def write_file(filename="", text=""):
     """writes a string to a text file"""
-    with open(filename, 'w') as f:
+    with open(filename, 'w+') as f:
         return f.write(text)

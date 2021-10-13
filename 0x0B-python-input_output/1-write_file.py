15cc7bd54b6843d5ad1adbf58e26c5b4ec200300
fabianatellechea <fabianatellechea@github.com>
2021-10-13 22:05:35 +0000

    .

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,5 +4,5 @@
 
 def write_file(filename="", text=""):
     """writes a string to a text file"""
-    with open(filename, 'w+') as f:
+    with open(filename, 'w') as f:
         return f.write(text)
